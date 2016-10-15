16694d19ebcd6a4da0cba8b811f3e90536498a29
Najafa Islam <inajafa@gmail.com>
2016-10-15 12:18:50 -0600

    deleted this to find a more clever way
    tried setting an integer then found out foreloop

--- Challenge_3/Challenge_3.ino
+++ Challenge_3/Challenge_3.ino
@@ -5,17 +5,12 @@ void setup() {
 }
 
 void loop() {
+/*int x
+x >= 10
+x +=10; */
 sparki.clearLCD();
-sparki.drawChar (0,0, '1');
-sparki.drawChar (0,1, '2');
-sparki.drawChar (0,2, '3');
-sparki.drawChar (0,3, '4');
-sparki.drawChar (0,4, '5');
-sparki.drawChar (0,5, '6');
-sparki.drawChar (0,6, '7');
-sparki.drawChar (0,7, '8');
-sparki.drawChar (0,8, '9');
-sparki.drawChar (0,9, '10');
+sparki.drawChar (0,0, x);
+
 
 sparki.updateLCD();
 delay(500);
