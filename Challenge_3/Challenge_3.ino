135b7f854e733a576ce585ad75f9883062f491b7
Najafa Islam <inajafa@gmail.com>
2016-10-15 12:02:16 -0600

    entered digits 5-10 then updated lcd

--- Challenge_3/Challenge_3.ino
+++ Challenge_3/Challenge_3.ino
@@ -11,5 +11,13 @@ sparki.drawChar (0,1, '2');
 sparki.drawChar (0,2, '3');
 sparki.drawChar (0,3, '4');
 sparki.drawCHar (0,4, '5');
+sparki.drawChar (0,5, '6');
+sparki.drawChar (0,6, '7');
+sparki.drawChar (0,7, '8');
+sparki.drawChar (0,8, '9');
+sparki.drawChar (0,9, '10');
+
+sparki.updateLCD();
+delay(500);
 
 }
