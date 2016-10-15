144255e21d3fe185ebb9166c552eae5ed87974bc
Najafa Islam <inajafa@gmail.com>
2016-10-15 11:59:03 -0600

    added digits 1-5

--- Challenge_3/Challenge_3.ino
+++ Challenge_3/Challenge_3.ino
@@ -7,5 +7,9 @@ void setup() {
 void loop() {
 sparki.clearLCD();
 sparki.drawChar (0,0, '1');
+sparki.drawChar (0,1, '2');
+sparki.drawChar (0,2, '3');
+sparki.drawChar (0,3, '4');
+sparki.drawCHar (0,4, '5');
 
 }
