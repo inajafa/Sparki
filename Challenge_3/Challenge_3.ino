72c5558ec511806d93f4bbb42949019f5396f949
Najafa Islam <inajafa@gmail.com>
2016-10-15 12:34:05 -0600

    trying to add two foreloops together to change coordinate and number

--- Challenge_3/Challenge_3.ino
+++ Challenge_3/Challenge_3.ino
@@ -8,8 +8,11 @@ void loop() {
 /*int x
 x >= 10
 x +=10; */
+for (int x = 0; x < 10; x=x+1);
+{ for (int y = 0;, y 9; y=y+1);
+{
 sparki.clearLCD();
-sparki.drawChar (0,0, x);
+sparki.drawChar (0,y, x); }}
 
 
 sparki.updateLCD();
