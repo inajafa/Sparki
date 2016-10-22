a8f0f9a5a0277eae91321f010ed7530bf6754844
Najafa Islam <inajafa@gmail.com>
2016-10-22 09:17:34 -0600

    syntax errors

--- Challenge_3/Challenge_3.ino
+++ Challenge_3/Challenge_3.ino
@@ -5,15 +5,14 @@ void setup() {
 }
 
 void loop() {
-int x
-for (int x = 0; x < 10; x=x+1);
-{ int y
-for (int y = 0;, y < 9; y=y+1);
-{
-sparki.clearLCD();
-sparki.drawChar (0,y, x); 
-sparki.updateLCD();
-delay(500);
-}
-}
+  int x;
+    for (int x = 0; x < 10; x=x+1){
+      int y;
+      for (int y = 0; y < 9; y=y+1){
+        sparki.clearLCD();
+        sparki.drawChar (0,y, x); 
+        sparki.updateLCD();
+        delay(500);
+    }
+  }
 }
