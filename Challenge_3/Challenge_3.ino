8d3dd89d6755f0a618156135e359662ed0fe64f4
Najafa Islam <inajafa@gmail.com>
2016-10-22 10:41:59 -0600

    made animation instead

--- Challenge_3/Challenge_3.ino
+++ Challenge_3/Challenge_3.ino
@@ -1,18 +1,23 @@
 #include <Sparki.h>
+#include <stdio.h>
+
 void setup() {
 
 
 }
 
 void loop() {
-  int x;
-    for (int x = 0; x < 10; x=x+1){
-      int y;
-      for (int y = 0; y < 9; y=y+1){
-        sparki.clearLCD();
-        sparki.drawChar (0,y, x); 
+sparki.clearLCD();
+    for (int x = 0; x < 10; x++){
+      for (int y = 0; y < 9; y++){
+        
+    
+        sparki.print("\n");
+        sparki.println(x);
         sparki.updateLCD();
         delay(500);
     }
   }
 }
+
+
