9dfb87bb7457b3ff9b3a41536750c9b90781fd1d
Najafa Islam <inajafa@gmail.com>
2016-10-21 18:11:15 -0600

    Attempted 2 for loops within each other, but syntax error

--- Challenge_3/Challenge_3.ino
+++ Challenge_3/Challenge_3.ino
@@ -5,17 +5,15 @@ void setup() {
 }
 
 void loop() {
-/*int x
-x >= 10
-x +=10; */
+int x
 for (int x = 0; x < 10; x=x+1);
-{ for (int y = 0;, y 9; y=y+1);
+{ int y
+for (int y = 0;, y < 9; y=y+1);
 {
 sparki.clearLCD();
-sparki.drawChar (0,y, x); }}
-
-
+sparki.drawChar (0,y, x); 
 sparki.updateLCD();
 delay(500);
-
+}
+}
 }
