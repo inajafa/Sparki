a93cba0409e1446760037ff6bf8912e81da8dac4
Najafa Islam <inajafa@gmail.com>
2016-10-22 11:23:51 -0600

    attempted but animation is still reversed

--- Challenge_3/Challenge_3.ino
+++ Challenge_3/Challenge_3.ino
@@ -12,11 +12,11 @@ sparki.clearLCD();
       for (int y = 0; y < 9; y++){
         
     
-        
         sparki.println(x);
         sparki.print("\n");
-        sparki.updateLCD();
+        
         delay(500);
+        sparki.updateLCD();
     }
   }
 }
