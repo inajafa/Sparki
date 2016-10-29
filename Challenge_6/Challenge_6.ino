edbeee58448e3a8578d048ca0210450245492d32
Najafa Islam <inajafa@gmail.com>
2016-10-29 12:37:56 -0600

    added rectangle values

--- Challenge_6/Challenge_6.ino
+++ Challenge_6/Challenge_6.ino
@@ -6,7 +6,7 @@ void setup() {
 
 void loop() {
 sparki.clearLCD();
-sparki.drawRect(5,5, 10,10);
-sparki.drawDrectFilled(20,22, 10,100);
+sparki.drawRect(5,5,10,10);
+sparki.drawRectFilled(5,5,10,10);
 
 }
