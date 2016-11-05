8e03cdc311a836bbc844882635efaa17c0c72d7b
Najafa Islam <inajafa@gmail.com>
2016-11-05 12:44:34 -0600

    we deleted the first four lines of code

--- Stars/Stars.ino
+++ Stars/Stars.ino
@@ -1,18 +1,15 @@
 #include <Sparki.h>
-void setup() {
-sparki.moveForward(20);
-sparki.moveRight(108); 
-sparki.moveForward(20);
-sparki.moveRight(108); 
+void setup() { 
 sparki.moveForward(20);
 sparki.moveRight(144);
 sparki.moveForward(20);
+sparki.moveRight(144);
+sparki.moveForward (20);
 sparki.moveRight(144);
 sparki.moveForward (20);
 sparki.moveRight(144);
 sparki.moveForward (20);
 }
-
 void loop() {
 
 }
