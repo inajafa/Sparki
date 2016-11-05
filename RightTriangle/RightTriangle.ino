e4ebe3f40e2e3a25670bd7a0b60e47805794ef7a
Najafa Islam <inajafa@gmail.com>
2016-11-05 12:22:41 -0600

    realized making extra lines

--- RightTriangle/RightTriangle.ino
+++ RightTriangle/RightTriangle.ino
@@ -1,10 +1,10 @@
 #include <Sparki.h>
 void setup() {
-sparki.moveForward(20);
+sparki.moveForward(17);
 sparki.moveRight(150); 
 sparki.moveForward(20);
 sparki.moveRight(120); 
-sparki.moveForward(20);
+sparki.moveForward(10);
 
 }
 
