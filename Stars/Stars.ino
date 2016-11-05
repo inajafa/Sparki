eaef3332a9058fd1f4df1ac5da4c249d88486a96
Najafa Islam <inajafa@gmail.com>
2016-11-05 12:35:50 -0600

    outlier angles .. did inner before

---
+++ Stars/Stars.ino
@@ -0,0 +1,18 @@
+#include <Sparki.h>
+void setup() {
+sparki.moveForward(20);
+sparki.moveRight(108); 
+sparki.moveForward(20);
+sparki.moveRight(108); 
+sparki.moveForward(20);
+sparki.moveRight(144);
+sparki.moveForward(20);
+sparki.moveRight(144);
+sparki.moveForward (20);
+sparki.moveRight(144);
+sparki.moveForward (20);
+}
+
+void loop() {
+
+}
