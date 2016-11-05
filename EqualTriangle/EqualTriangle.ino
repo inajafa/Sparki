1f9bb2e7bd2096bcb8c8a90c0afa8f949daccb45
Najafa Islam <inajafa@gmail.com>
2016-11-05 12:15:00 -0600

    use the outier angle to make  an equal triangle

---
+++ EqualTriangle/EqualTriangle.ino
@@ -0,0 +1,17 @@
+#include <Sparki.h>
+void setup() {
+sparki.moveForward(20);
+sparki.moveRight(120); 
+sparki.moveForward(20);
+sparki.moveRight(120); 
+sparki.moveForward(20);
+sparki.moveRight(120); 
+sparki.moveForward(20);
+ 
+
+
+}
+
+void loop() {
+
+}
