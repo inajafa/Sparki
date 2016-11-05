ff20177f3c826bf42e199b6b0ebdf4e18ddd8586
Najafa Islam <inajafa@gmail.com>
2016-11-05 12:52:00 -0600

    guess and check never goes wrong

--- Stars/Stars.ino
+++ Stars/Stars.ino
@@ -7,8 +7,8 @@ sparki.moveRight(144);
 sparki.moveForward (20);
 sparki.moveRight(144);
 sparki.moveForward (20);
-sparki.moveRight(144);
-sparki.moveForward (20);
+sparki.moveRight(130);
+sparki.moveForward (15);
 }
 void loop() {
 
