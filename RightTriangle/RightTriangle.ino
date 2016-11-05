da14765df141acc9b364336422aed5864310ff58
Najafa Islam <inajafa@gmail.com>
2016-11-05 12:20:06 -0600

    realized we dont need to move 90 to make a right triangle.
    line too long tho

---
+++ RightTriangle/RightTriangle.ino
@@ -0,0 +1,13 @@
+#include <Sparki.h>
+void setup() {
+sparki.moveForward(20);
+sparki.moveRight(150); 
+sparki.moveForward(20);
+sparki.moveRight(120); 
+sparki.moveForward(20);
+
+}
+
+void loop() {
+
+}
